92cd780bfd53277491f18c821c618bb06851b22e
Igor Simões <igorsimoes_96@hotmail.com>
2024-03-09 17:05:05 +0000

    add models and controller for movies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies_SeriesAPI.Models;
+
+namespace Movies_SeriesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        //MoviesContext instance to communicate with DB
+        private readonly MoviesContext _context;
+
+        //constructor gets MoviesContext instance with dependency injection
+        public MoviesController(MoviesContext context)
+        {
+            //var to use that instance
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movies>> GetAllMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Movies> GetMovieById(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return movie;
+        }
+
+        [HttpPost]
+        public ActionResult<Movies> CreateMovie(Movies movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMovie(int id, Movies movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
